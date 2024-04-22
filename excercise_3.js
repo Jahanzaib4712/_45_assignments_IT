086b9524ea3d95b118fbc8893242e600eb63e354
Jahanzaib4712 <160598382+Jahanzaib4712@users.noreply.github.com>
2024-04-22 02:11:11 -0700

    Add files via upload

---
+++ excercise_3.js
@@ -0,0 +1,8 @@
+let personName = "sahir ali";
+// Print the person's name in lowercase:
+console.log("Lowercase", personName.toLowerCase());
+// // Print the person's name in uppercase:
+console.log("Uppercase", personName.toUpperCase());
+// // Print the person's name in titlecase :
+console.log("Titlecase", personName.charAt(0).toUpperCase() + personName.slice(1).toLowerCase());
+export {};
