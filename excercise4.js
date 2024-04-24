4f5b1c220015c6603ecd30cc34062f92a3ed2cc4
Jahanzaib4712 <160598382+Jahanzaib4712@users.noreply.github.com>
2024-04-24 00:28:11 -0700

    Add files via upload

---
+++ excercise4.js
@@ -0,0 +1,5 @@
+// Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+let Quote = "the only way to do great work is to love what you do ";
+let author = "steve jobs";
+console.log(author, "said", Quote);
+export {};
