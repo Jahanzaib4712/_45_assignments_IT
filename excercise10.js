43ec4af5651b7f2f95998023b50ef1fbc6c1f7ca
Jahanzaib4712 <160598382+Jahanzaib4712@users.noreply.github.com>
2024-04-24 00:49:40 -0700

    Add files via upload

---
+++ excercise10.js
@@ -0,0 +1,7 @@
+// Jahanzaib 
+// April 24, 2024
+// This program is demonstrates the basic arthmatic operations in Typescript.
+console.log(5 + 4); // Addition
+console.log(10 - 6); // Subtraction
+console.log(4 * 5); // Multiplication
+console.log(16 / 3); // Division10
