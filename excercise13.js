05a384bdc8b9a0abbc6772c1458b39175b7c348b
Jahanzaib4712 <160598382+Jahanzaib4712@users.noreply.github.com>
2024-04-27 03:32:44 -0700

    Add files via upload

---
+++ excercise13.js
@@ -0,0 +1,5 @@
+let motorcycles = ["Honda 125", "Yamaha YBR", "Kawasaki", "suzuki", "Harley Davidson"];
+motorcycles.forEach(motorcycle => {
+    console.log("i would like to own  " + motorcycle, "motorcycle");
+});
+export {};
