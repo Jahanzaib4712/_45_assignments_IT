9f596a2ab9dfe73f96ab65bf370e3a4cb8bfb944
Jahanzaib4712 <160598382+Jahanzaib4712@users.noreply.github.com>
2024-04-27 04:08:09 -0700

    Add files via upload

---
+++ excercise16.ts
@@ -0,0 +1,19 @@
+
+//exercise 16 : More Guests :
+/*More Guests: You just found a bigger dinner table, so now more space is available. Think of three more guests to invite to dinner.*/
+
+
+//solution:
+
+let moreGuests : String[] = ["Ali", "Umar", "farrukh", "sultan"];
+
+console.log("Guys!, i have found a bigger table for our dinner, so i am inviting more guests.");
+
+moreGuests.unshift("salahuddin ayubi");
+moreGuests.splice(3, 0, "usama");
+moreGuests.push("khalid");
+
+moreGuests.forEach(moreGuests => {
+    console.log(" hey " + moreGuests , "i would like to invite you to dinner tonight");
+
+});
