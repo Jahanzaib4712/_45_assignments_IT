806d3f149037f4af6c8c23739fd3b319bfd54f9b
Jahanzaib4712 <160598382+Jahanzaib4712@users.noreply.github.com>
2024-04-27 04:42:52 -0700

    Add files via upload

---
+++ excercise19.ts
@@ -0,0 +1,13 @@
+
+//exercise 18 : Seeing the World  :
+/* Think of at least five places in the world you’d like to visit. */
+
+
+//solution:
+
+import {  guests} from "./excercise14";
+
+
+console.log( ` at last, i have invited ${guests.length} peoples. `)
+
+
