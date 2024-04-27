880838964dcb176663cc732aa6f8bc05e4bd40b0
Jahanzaib4712 <160598382+Jahanzaib4712@users.noreply.github.com>
2024-04-27 03:57:58 -0700

    Add files via upload

---
+++ excercise15.js
@@ -0,0 +1,15 @@
+// Excercise 15 : 
+/* Changing Guest List: You just heard that one of your guests can’t make the dinner,
+ so you need to send out a new set of invitations. You’ll have to think of someone else to invite.*/
+let guestList = ["Albert Einstein", "Imran Khan ", "Marie Curie"];
+// Assuming Marie Curie can't make it, let's replace her with Nikola Tesla
+let newGuestList = [...guestList]; // Copy the original list
+let indexOfMarieCurie = newGuestList.indexOf("Marie Curie");
+if (indexOfMarieCurie !== -1) {
+    newGuestList.splice(indexOfMarieCurie, 1, "Tony Stark");
+}
+// Print the new set of invitations
+newGuestList.forEach(guest => {
+    console.log(`Aziz ${guest}, main aapko dinner ke liye invite karna chahta hoon. Agar aap humare saath shamil hona chahte hain toh please mujhe bataiye.`);
+});
+export {};
