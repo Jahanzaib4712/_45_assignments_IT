558166dc193c5dc25996f122493d23f1c5cfd829
Jahanzaib4712 <160598382+Jahanzaib4712@users.noreply.github.com>
2024-04-27 04:48:03 -0700

    Add files via upload

---
+++ excercise20.js
@@ -0,0 +1,11 @@
+//exercise 18 : Think of something you could store in a array  :
+/* Think of something you could store in a array. For example,
+you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+*/
+//solution:
+let cities = ["Karachi", "Mumbai", "Delhi", "Lahore", "Islamabad"];
+console.log("list of Cities is given below:");
+for (let list of cities) {
+    console.log(list);
+}
+export {};
