5174cefe5502414001e601433e8451321197c412
Jahanzaib4712 <160598382+Jahanzaib4712@users.noreply.github.com>
2024-04-27 03:44:43 -0700

    Add files via upload

---
+++ excercise14.js
@@ -0,0 +1,10 @@
+// excercise 14 :
+/* Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite?
+Make a list that includes at least three people you’d like to invite to dinner.
+ Then use your list to print a message to each person, inviting them to dinner.*/
+// Solution : 
+let guests = ["albert einstein", "Imran khan ", "Haroon Kadwani", "Tony Stark"];
+guests.forEach(guest => {
+    console.log(" hey " + guest, "i would like to invite you to dinner tonight");
+});
+export {};
