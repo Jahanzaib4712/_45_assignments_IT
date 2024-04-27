8afcaebbededf1d1ea4826cbc3bc1ea5ed1b58ca
Jahanzaib4712 <160598382+Jahanzaib4712@users.noreply.github.com>
2024-04-27 02:55:51 -0700

    Add files via upload

---
+++ excercise11.js
@@ -0,0 +1,6 @@
+//Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+let names = ["ali", "umer", "ahmed"];
+names.forEach(name => {
+    console.log(name);
+});
+export {};
---
+++ excercise11.ts
@@ -0,0 +1,9 @@
+//Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+
+let names : string[] =[ "ali", "umer", "ahmed"];
+
+names.forEach(name=> {
+    console.log(name);
+});
+
+
