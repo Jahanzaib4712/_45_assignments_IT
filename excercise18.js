e5a5d795418d757648ddf541584dcee8731c217f
Jahanzaib4712 <160598382+Jahanzaib4712@users.noreply.github.com>
2024-04-27 04:38:19 -0700

    Add files via upload

---
+++ excercise18.js
@@ -0,0 +1,19 @@
+//exercise 18 : Seeing the World  :
+/* Think of at least five places in the world you’d like to visit. */
+//solution:
+//exercise 18: seeing the world:
+//solution:
+let places = ['Maldives', 'Italy', 'Japan', 'Peru', 'Finland'];
+console.log(`Original order:`, places);
+let sortedPlaces = ([...places].sort());
+console.log(`Alphabetical sorted copy:`, sortedPlaces);
+let originalArr = [...places];
+console.log(`Original order:`, originalArr);
+let reversedPlaces = ([...places].sort().reverse());
+console.log(`Reversed alphabetical copy:`, reversedPlaces);
+console.log(`Original order:`, originalArr);
+console.log(`Original order:`, places.reverse());
+console.log(`Original order:`, places.reverse());
+console.log(`Original order:`, [...places].sort());
+console.log(`Original order:`, [...places].sort().reverse());
+export {};
